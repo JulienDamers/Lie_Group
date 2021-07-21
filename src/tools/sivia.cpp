03453502dfff066a21bf6de1257e74f25414428a
JD <julien.damers@orange.fr>
2021-07-21 15:24:11 +0200

    added colorblind friendly colors to SIVIA tool + number of bissections indication

--- src/tools/sivia.cpp
+++ src/tools/sivia.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "sivia.h"
+//#include "figure.h"
 
 using namespace ibex;
 using namespace codac;
@@ -10,10 +11,16 @@ using namespace vibes;
 using namespace pyibex;
 
 
-void sivia(IntervalVector& map, SepProj fullSep, double epsilon)
+
+void sivia(IntervalVector& map, SepProj& fullSep, double epsilon)
 {
+
+    int boxes_counter = 1;
+    int bissection = 0;
+
     stack<IntervalVector> s;
     s.push(map);
+
 
     while(!s.empty())
     {
@@ -24,11 +31,11 @@ void sivia(IntervalVector& map, SepProj fullSep, double epsilon)
         fullSep.separate(boxIn,boxOut);
         if (boxOut[0].is_empty() && boxOut[1].is_empty())
         {
-            drawBox(box[0].lb(),box[0].ub(), box[1].lb(),box[1].ub(), "b[c]");
+            drawBox(box[0].lb(),box[0].ub(), box[1].lb(),box[1].ub(), "b[#648FFF]");
         }
         else if (boxIn[0].is_empty() && boxIn[1].is_empty())
         {
-            drawBox(box[0].lb(),box[0].ub(), box[1].lb(),box[1].ub(), "r[m]");
+            drawBox(box[0].lb(),box[0].ub(), box[1].lb(),box[1].ub(), "r[#DC267F]");
         }
         else
         {
@@ -38,13 +45,60 @@ void sivia(IntervalVector& map, SepProj fullSep, double epsilon)
                 pair<IntervalVector, IntervalVector> p = box.bisect(i);
                 s.push(p.first);
                 s.push(p.second);
+                bissection++;
             }
             else
             {
-                drawBox(box[0].lb(),box[0].ub(),box[1].lb(),box[1].ub(),"k[w]");
+                drawBox(box[0].lb(),box[0].ub(),box[1].lb(),box[1].ub(),"y[#FFB000]");
 
             }
         }
 
     }
+    cout << bissection << " bissections have been done" << endl;
+}
+
+
+void sivia_article(ibex::IntervalVector& map, pyibex::SepProj& fullSep, double epsilon, ipegenerator::Figure& fig)
+{
+    stack<IntervalVector> s;
+    s.push(map);
+
+    int bissection = 0;
+
+    while(!s.empty())
+    {
+        IntervalVector box = s.top();
+        s.pop();
+        IntervalVector boxIn(box);
+        IntervalVector boxOut(box);
+        fullSep.separate(boxIn,boxOut);
+        if (boxOut[0].is_empty() && boxOut[1].is_empty())
+        {
+            fig.draw_box(box,"blue","colorBlindOut");
+        }
+        else if (boxIn[0].is_empty() && boxIn[1].is_empty())
+        {
+            fig.draw_box(box,"red","colorBlindIn");
+        }
+        else
+        {
+            if (box.max_diam()>epsilon)
+            {
+                int i = box.subvector(0,1).extr_diam_index(false);
+                pair<IntervalVector, IntervalVector> p = box.bisect(i);
+                s.push(p.first);
+                s.push(p.second);
+                bissection++;
+            }
+            else
+            {
+                fig.draw_box(box,"gold","colorBlindMaybe");
+
+            }
+        }
+
+    }
+    cout << bissection << " bissections have been done" << endl;
+
 }
--- src/tools/sivia.h
+++ src/tools/sivia.h
@@ -4,7 +4,10 @@
 #include "pyibex_SepProj.h"
 #include "codac.h"
 #include "codac-rob.h"
+#include "ipegenerator.h"
 
-void sivia(ibex::IntervalVector& map, pyibex::SepProj fullSep, double epsilon);
+void sivia(ibex::IntervalVector& map, pyibex::SepProj& fullSep, double epsilon);
+
+void sivia_article(ibex::IntervalVector& map, pyibex::SepProj& fullSep, double epsilon, ipegenerator::Figure& fig);
 
 #endif
