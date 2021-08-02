a08a55f15b3ded19ddf95437b9fedb0bde574863
JD <julien.damers@orange.fr>
2021-08-02 17:45:31 +0200

    added sivia for contractors

--- src/tools/sivia.cpp
+++ src/tools/sivia.cpp
@@ -12,7 +12,7 @@ using namespace pyibex;
 
 
 
-void sivia(IntervalVector& map, SepProj& fullSep, double epsilon)
+void sivia(IntervalVector& map, Sep& Sep, double epsilon)
 {
 
     int boxes_counter = 1;
@@ -28,7 +28,7 @@ void sivia(IntervalVector& map, SepProj& fullSep, double epsilon)
         s.pop();
         IntervalVector boxIn(box);
         IntervalVector boxOut(box);
-        fullSep.separate(boxIn,boxOut);
+        Sep.separate(boxIn,boxOut);
         if (boxOut[0].is_empty() && boxOut[1].is_empty())
         {
             drawBox(box[0].lb(),box[0].ub(), box[1].lb(),box[1].ub(), "b[#648FFF]");
@@ -58,8 +58,48 @@ void sivia(IntervalVector& map, SepProj& fullSep, double epsilon)
     cout << bissection << " bissections have been done" << endl;
 }
 
+void sivia(IntervalVector& map, Ctc& Ctc, double epsilon)
+{
 
-void sivia_article(ibex::IntervalVector& map, pyibex::SepProj& fullSep, double epsilon, ipegenerator::Figure& fig)
+    int bissection = 0;
+
+    stack<IntervalVector> s;
+    s.push(map);
+
+
+    while(!s.empty())
+    {
+        IntervalVector box = s.top();
+        s.pop();
+        IntervalVector boxOut(box);
+        Ctc.contract(boxOut);
+        if (boxOut[0].is_empty() && boxOut[1].is_empty())
+        {
+            drawBox(box[0].lb(),box[0].ub(), box[1].lb(),box[1].ub(), "b[#648FFF]");
+        }
+        else
+        {
+            if (box.max_diam()>epsilon)
+            {
+                int i = box.subvector(0,1).extr_diam_index(false);
+                pair<IntervalVector, IntervalVector> p = box.bisect(i);
+                s.push(p.first);
+                s.push(p.second);
+                bissection++;
+            }
+            else
+            {
+                drawBox(box[0].lb(),box[0].ub(),box[1].lb(),box[1].ub(),"y[#FFB000]");
+
+            }
+        }
+
+    }
+    cout << bissection << " bissections have been done" << endl;
+}
+
+
+void sivia_article(ibex::IntervalVector& map, ibex::Sep& Sep, double epsilon, ipegenerator::Figure& fig)
 {
     stack<IntervalVector> s;
     s.push(map);
@@ -72,7 +112,7 @@ void sivia_article(ibex::IntervalVector& map, pyibex::SepProj& fullSep, double e
         s.pop();
         IntervalVector boxIn(box);
         IntervalVector boxOut(box);
-        fullSep.separate(boxIn,boxOut);
+        Sep.separate(boxIn,boxOut);
         if (boxOut[0].is_empty() && boxOut[1].is_empty())
         {
             fig.draw_box(box,"blue","colorBlindOut");
@@ -102,3 +142,43 @@ void sivia_article(ibex::IntervalVector& map, pyibex::SepProj& fullSep, double e
     cout << bissection << " bissections have been done" << endl;
 
 }
+
+void sivia_article(ibex::IntervalVector& map, ibex::Ctc& Ctc, double epsilon, ipegenerator::Figure& fig)
+{
+    stack<IntervalVector> s;
+    s.push(map);
+
+    int bissection = 0;
+
+    while(!s.empty())
+    {
+        IntervalVector box = s.top();
+        s.pop();
+        IntervalVector boxOut(box);
+        Ctc.contract(boxOut);
+        if (boxOut[0].is_empty() && boxOut[1].is_empty())
+        {
+            fig.draw_box(box,"blue","colorBlindOut");
+        }
+        else
+        {
+            if (box.max_diam()>epsilon)
+            {
+                int i = box.subvector(0,1).extr_diam_index(false);
+                pair<IntervalVector, IntervalVector> p = box.bisect(i);
+                s.push(p.first);
+                s.push(p.second);
+                bissection++;
+            }
+            else
+            {
+                fig.draw_box(box,"gold","colorBlindMaybe");
+
+            }
+        }
+
+    }
+    cout << bissection << " bissections have been done" << endl;
+
+}
+
--- src/tools/sivia.h
+++ src/tools/sivia.h
@@ -6,8 +6,9 @@
 #include "codac-rob.h"
 #include "ipegenerator.h"
 
-void sivia(ibex::IntervalVector& map, pyibex::SepProj& fullSep, double epsilon);
+void sivia(ibex::IntervalVector& map, ibex::Sep& Sep, double epsilon);
+void sivia(ibex::IntervalVector& map, ibex::Ctc& Ctc, double epsilon);
 
-void sivia_article(ibex::IntervalVector& map, pyibex::SepProj& fullSep, double epsilon, ipegenerator::Figure& fig);
-
+void sivia_article(ibex::IntervalVector& map, ibex::Sep& Sep, double epsilon, ipegenerator::Figure& fig);
+void sivia_article(ibex::IntervalVector& map, ibex::Ctc& Ctc, double epsilon, ipegenerator::Figure& fig);
 #endif
