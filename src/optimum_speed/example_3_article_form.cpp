42f5866baacaff9faab483d266fb3a668ae0bd48
JD <julien.damers@orange.fr>
2021-08-05 16:11:32 +0200

    [FIGURES] added discrete version to example 3 and 4

--- src/optimum_speed/example_3_article_form.cpp
+++ src/optimum_speed/example_3_article_form.cpp
@@ -18,14 +18,8 @@ using namespace ibex;
 using namespace pyibex;
 
 
-
-
-
-int main(int argc, char* argv[])
+void example_3_continuous_article()
 {
-    Tube::enable_syntheses();
-
-
     Interval domain(0, 4);
     double timestep = 0.01;
     IntervalVector x0(2);
@@ -68,6 +62,93 @@ int main(int argc, char* argv[])
     fig.set_number_digits_axis_x(1);
     fig.set_number_digits_axis_y(1);
     fig.draw_axis("x1","x2");
-    fig.save_ipe("example_3.ipe");
-    fig.save_pdf("example_3.pdf");
+    fig.save_ipe("example_3_continuous.ipe");
+    fig.save_pdf("example_3_continuous.pdf");
+
+    return;
+}
+
+void example_3_discrete_article()
+{
+    Interval domain(0, 4);
+    double timestep = 0.01;
+    IntervalVector x0(2);
+    x0[0] = Interval(0.5, 0.5);
+    x0[1] = Interval(0, 0);
+    // Generate the reference  as we do not have a analytical expression for it
+    Function f("x", "y", "(x^3+x*y^2-x+y; y^3+x^2*y-x-y)");
+    // CAPD integration version
+    TubeVector a = CAPD_integrateODE(domain, f, x0, timestep);
+    cout << "Reference generated " << a << endl;
+
+    double epsilon = 0.01; // precision of the sivia
+
+    IntervalVector X0({{0.4,  0.6},
+                       {-0.1, 0.1}}); // Starting box constraint
+    IntervalVector x({{-1.2, 1.2},
+                      {-1.2, 1.2}}); // Space to be explored
+    lie_group_ex3_separator* fullSep;
+    fullSep = new lie_group_ex3_separator(&a, &X0);
+    vector<float> projection_times{0.,1.,2.,3.,4.};
+
+    vector<Sep*> seps;
+    vector<IntervalVector*> projections;
+    for (size_t i=0;i<projection_times.size();i++)
+    {
+        IntervalVector *proj = new IntervalVector(1); // Defining interval to project
+        (*proj)[0] = Interval(projection_times[i],projection_times[i]);
+        projections.push_back(proj);
+        SepProj *sepProj = new SepProj(*fullSep,*proj,epsilon);
+        seps.push_back(sepProj);
+
+    }
+    Array<Sep> ar_sep(seps);
+    SepUnion usep (ar_sep);
+
+    IntervalVector frame({{-1.2,1.2},{-1.2,1.2}});
+    ipegenerator::Figure fig(frame,150,150);
+
+
+    auto start = chrono::steady_clock::now();
+    sivia_article(x, usep, epsilon,fig);
+    auto stop = chrono::steady_clock::now();
+    cout << "elapsed time: " << chrono::duration_cast<chrono::milliseconds>(stop - start).count() << " ms" << endl;
+
+    fig.set_color_stroke("black");
+    fig.set_color_type(ipegenerator::STROKE_ONLY);
+    fig.draw_circle(0,0,1);
+    fig.set_opacity(30);
+    fig.draw_box(X0, "green","green");
+    fig.set_opacity(100);
+    fig.draw_tubeVector(&a,0,1,"black","black",ipegenerator::STROKE_AND_FILL);
+    fig.set_graduation_parameters(-1.5,0.5,-1.5,0.5);
+    fig.set_number_digits_axis_x(1);
+    fig.set_number_digits_axis_y(1);
+    fig.draw_axis("x1","x2");
+    fig.save_ipe("example_3_discrete.ipe");
+    fig.save_pdf("example_3_discrete.pdf");
+
+    int n = seps.size();
+    for (int i =0; i<n; i++)
+    {
+        delete(projections[i]);
+        delete(seps[i]);
+    }
+
+    return;
+}
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+    Tube::enable_syntheses();
+
+    example_3_continuous_article();
+    example_3_discrete_article();
+
+
+
 }
--- src/optimum_speed/example_4_article_form.cpp
+++ src/optimum_speed/example_4_article_form.cpp
@@ -16,13 +16,8 @@ using namespace codac;
 using namespace ibex;
 using namespace pyibex;
 
-
-int main(int argc, char* argv[])
+void example_4_continuous_article()
 {
-
-    Tube::enable_syntheses();
-
-
     Interval domain(0,15);
     double timestep = 0.001;
     IntervalVector x0(4);
@@ -57,13 +52,101 @@ int main(int argc, char* argv[])
     cout << "elapsed time: " << chrono::duration_cast<chrono::milliseconds>(stop - start).count() << " ms" <<endl;
 
     fig.set_opacity(30);
-    fig.draw_box(x0,"green","green");
+    fig.draw_box(X0.subvector(0,1),"green","green");
     fig.set_opacity(100);
     fig.draw_tubeVector(&a,0,1,"black","black",ipegenerator::STROKE_AND_FILL);
     fig.set_graduation_parameters(-4,0.5,-4,0.5);
     fig.set_number_digits_axis_x(1);
     fig.set_number_digits_axis_y(1);
     fig.draw_axis("x1","x2");
-    fig.save_ipe("example_4.ipe");
-    fig.save_pdf("example_4.pdf");
+    fig.save_ipe("example_4_continuous.ipe");
+    fig.save_pdf("example_4_continuous.pdf");
+    return;
+}
+
+void example_4_discrete_article()
+{
+    Interval domain(0,15);
+    double timestep = 0.001;
+    IntervalVector x0(4);
+    x0.init(Interval(0,0));
+    Function f("x","y","z","x4","(cos(z); sin(z); sin(0.4*x4); 1)");
+    // CAPD integration version
+    TubeVector a = CAPD_integrateODE(domain,f,x0,timestep);
+    cout << "Reference generated " << a << endl;
+
+    double epsilon = 0.01;
+
+    IntervalVector X0(3);
+    X0[0] = Interval(-0.1,0.1);
+    X0[1] = Interval(-0.1,0.1);
+    X0[2] = Interval(-0.4,0.4);
+
+    lie_group_ex4_separator* fullSep;
+    fullSep = new lie_group_ex4_separator(&a,&X0);
+
+    vector<IntervalVector> projection_times{
+            IntervalVector({{-6,6},{1,1}}),
+            IntervalVector({{-6,6},{2,2}}),
+            IntervalVector({{-6,6},{3,3}}),
+            IntervalVector({{-6,6},{4,4}}),
+            IntervalVector({{-6,6},{14,14}}),
+            IntervalVector({{-6,6},{15,15}})
+    };
+
+    vector<Sep*> seps;
+    vector<IntervalVector*> projections;
+    for (size_t i=0;i<projection_times.size();i++)
+    {
+        IntervalVector *proj = new IntervalVector(2); // Defining interval to project
+        (*proj) = projection_times[i];
+        projections.push_back(proj);
+        SepProj *sepProj = new SepProj(*fullSep,*proj,epsilon);
+        seps.push_back(sepProj);
+
+    }
+
+    Array<Sep> ar_sep(seps);
+    SepUnion usep (ar_sep);
+
+
+    IntervalVector x({{-4,4},{-4,4}});
+
+    IntervalVector frame({{-4,4},{-4,4}});
+    ipegenerator::Figure fig(frame,150,150);
+
+    auto start = chrono::steady_clock::now();
+    sivia_article(x,usep,epsilon,fig);
+    auto stop = chrono::steady_clock::now();
+    cout << "elapsed time: " << chrono::duration_cast<chrono::milliseconds>(stop - start).count() << " ms" <<endl;
+
+    fig.set_opacity(30);
+    fig.draw_box(X0.subvector(0,1),"green","green");
+    fig.set_opacity(100);
+    fig.draw_tubeVector(&a,0,1,"black","black",ipegenerator::STROKE_AND_FILL);
+    fig.set_graduation_parameters(-4,0.5,-4,0.5);
+    fig.set_number_digits_axis_x(1);
+    fig.set_number_digits_axis_y(1);
+    fig.draw_axis("x1","x2");
+    fig.save_ipe("example_4_discrete.ipe");
+    fig.save_pdf("example_4_discrete.pdf");
+
+    int n = seps.size();
+    for (int i =0; i<n; i++)
+    {
+        delete(projections[i]);
+        delete(seps[i]);
+    }
+    return;
+}
+
+
+int main(int argc, char* argv[])
+{
+
+    Tube::enable_syntheses();
+    example_4_continuous_article();
+    example_4_discrete_article();
+
+
 }
