7525d549e8b54aaf7934e21d7852f969b65a429f
JD <julien.damers@orange.fr>
2021-10-02 11:29:06 +0200

    [REVIEW] finished reachability refurbishement on ex 4

--- src/comparisons_review_2/integration_ex_4.cpp
+++ src/comparisons_review_2/integration_ex_4.cpp
@@ -36,26 +36,6 @@ void example_4(bool lohner_done, bool capd_done)
                          {0,    0}});
     Function f_4("x", "y", "z", "t", "(cos(z); sin(z); sin(0.4*t); 1)");
 
-
-    // Integration using Lohner
-    TubeVector x_lohner_4 = TubeVector(domain_4, timestep_4, 4);
-    CtcLohner ctc_lohner_4(f_4);
-    x_lohner_4.set(x0_4, 0.);
-    try
-    {
-        start = chrono::steady_clock::now();
-        ctc_lohner_4.contract(x_lohner_4);
-        stop = chrono::steady_clock::now();
-        cout << "Lohner integration ex 4 processed in : "
-             << chrono::duration_cast<chrono::milliseconds>(stop - start).count() << " ms"
-             << endl;
-        lohner_done = true;
-    }
-    catch ( exception &e )
-    {
-        lohner_done = false;
-        cout << "\n\nException caught!\n" << e.what() << endl;
-    }
     // Integration using CAPD
 
     TubeVector x_capd_4 = TubeVector(domain_4, timestep_4, 4);
@@ -83,6 +63,11 @@ void example_4(bool lohner_done, bool capd_done)
                              {0.,  0.},
                              {0.,  0.}});
     TubeVector a_lie_4 = CAPD_integrateODE(domain_4, f_4, x0_lie_4, timestep_4);
+    TubeVector reference(domain_4,timestep_4,3); // Generate a 3-dimension reference
+    reference[0] = a_lie_4[0];
+    reference[1] = a_lie_4[1];
+    reference[2] = a_lie_4[2];
+
     TubeVector x_lie_4(Interval(0,15), timestep_4,3);
 
 
@@ -92,33 +77,25 @@ void example_4(bool lohner_done, bool capd_done)
 
 
     // Define all necessary contractors
-    ibex::CtcFwdBwd ctc_sub(*new ibex::Function("x", "y", "z", "x-y-z"));
-
-
-    ibex::Variable x_v(4, "x_simp"); // x_simp=(x1,x2,x3,x4)
-    ibex::Variable w_v(4, "w"); // a(x4)
-    ibex::Variable beta_v(1, "beta"); // x3-a3(x4)
-
-    ibex::Function phi_simplified_4(x_v, w_v, beta_v,
-                                    Return(x_v[0] + cos(beta_v[0]) * (-w_v[0]) - sin(beta_v[0]) * (-w_v[1]),
-                                           x_v[1] + sin(beta_v[0]) * (-w_v[0]) + cos(beta_v[0]) * (-w_v[1]),
-                                           beta_v[0]));
-    ibex::CtcFwdBwd ctc_phi_simp_4(phi_simplified_4, X0_simplified_4);
+    ibex::CtcFwdBwd ctc_sub(*new ibex::Function("x[4]", "y[3]", "z", "x[3]-y[2]-z"));
+    ibex::Function phi("x[4]","w[3]","b","( x[1] + cos(b) * (-w[0]) - sin(b) * (-w[1]) - [-0.1,0.1]; x[2] + sin(b) * (-w[0]) + cos(b) * (-w[1]) - [-0.1,0.1]; b - [-0.4,0.4] )");
+    CtcFunction ctc_phi(phi);
     codac::CtcEval ctc_eval;
 
 
     // simplified version
     ContractorNetwork cn_simplified_out_4;
     IntervalVectorVar box_simp_out_4(4);
-    IntervalVector& w_simp_out_4 = cn_simplified_out_4.create_interm_var(IntervalVector(4));
+    IntervalVector& w_simp_out_4 = cn_simplified_out_4.create_interm_var(IntervalVector(3));
     Interval& beta_simp_out_4 = cn_simplified_out_4.create_interm_var( Interval());
-    cn_simplified_out_4.add(ctc_eval, {box_simp_out_4[3], w_simp_out_4, a_lie_4});
-    cn_simplified_out_4.add(ctc_sub, {box_simp_out_4[2], w_simp_out_4[2], beta_simp_out_4});
-    cn_simplified_out_4.add(ctc_phi_simp_4, {box_simp_out_4, w_simp_out_4, beta_simp_out_4});
+    cn_simplified_out_4.add(ctc_eval, {box_simp_out_4[0], w_simp_out_4, reference});
+    cn_simplified_out_4.add(ctc_sub, {box_simp_out_4, w_simp_out_4, beta_simp_out_4});
+    cn_simplified_out_4.add(ctc_phi, {box_simp_out_4, w_simp_out_4, beta_simp_out_4});
     ctc_cn ctc_cn_out_4(&cn_simplified_out_4, &box_simp_out_4);
+    CtcStatic ctc_static(ctc_cn_out_4,true);
 
     start = chrono::steady_clock::now();
-    ctc_cn_out_4.contract(x_lie_4);
+    ctc_static.contract(x_lie_4);
     stop = chrono::steady_clock::now();
 
     cout << x_lie_4(0) << endl;
@@ -133,35 +110,24 @@ void example_4(bool lohner_done, bool capd_done)
     fig_4.set_number_digits_axis_x(1);
     fig_4.set_number_digits_axis_y(1);
 
-    // Visuals initialization
-    if ( lohner_done )
-    {
-        fig_4.set_color_stroke("red");
-        fig_4.set_color_fill("red");
-        fig_4.set_opacity(30);
-        fig_4.set_color_type(ipegenerator::STROKE_AND_FILL);
-        //fig_4.draw_tubeVector(&x_lohner_4, 0, 1);
-        lohner_done = false;
-    }
     if ( capd_done )
     {
         fig_4.set_color_stroke("blue");
         fig_4.set_color_fill("blue");
         fig_4.set_opacity(30);
         fig_4.set_color_type(ipegenerator::STROKE_AND_FILL);
-        //fig_4.draw_tubeVector(&x_capd_4, 0, 1);
+        fig_4.draw_tubeVector(&x_capd_4,"x_capd", 0, 1);
         capd_done = false;
     }
     fig_4.set_opacity(30);
-    fig_4.set_color_stroke("yellow");
+    fig_4.set_color_stroke("red");
     fig_4.set_color_fill("yellow");
     fig_4.set_opacity(30);
     fig_4.set_color_type(ipegenerator::STROKE_AND_FILL);
-    fig_4.draw_tubeVector(&x_lie_4, 0, 1);
+    fig_4.draw_tubeVector(&x_lie_4,"x_lie", 0, 1);
     fig_4.set_opacity(30);
     fig_4.draw_box(x0_4.subvector(0, 1), "green", "green");
     fig_4.set_opacity(100);
-    fig_4.draw_circle(1.2,1.2,0.1);
     fig_4.add_layer("text");
     fig_4.set_current_layer("text");
     fig_4.set_color_stroke("black");
--- src/comparisons_review_2/reachability.cpp
+++ src/comparisons_review_2/reachability.cpp
@@ -8,6 +8,7 @@
 #include "tools.h"
 #include "chrono"
 #include "ctc_cn.h"
+#include "ipegenerator.h"
 
 
 
@@ -17,135 +18,165 @@ using namespace ibex;
 using namespace vibes;
 using namespace pyibex;
 
-void example_4(bool lohner_done, bool capd_done)
+void reachability()
 {
     cout << "##########################" << endl;
     cout << "########Example 4#########" << endl;
     cout << "##########################" << endl << endl;
 
-    auto start = chrono::steady_clock::now();
-    auto stop = chrono::steady_clock::now();
-
-
-    Interval domain_4(0, 15);
+    Interval domain_4(0,15);
     double timestep_4 = 0.01;
-    IntervalVector x0_4({{-0.1, 0.1},
-                         {-0.1, 0.1},
-                         {-0.4, 0.4},
-                         {0,    0}});
-    Function f_4("x", "y", "z", "t", "(cos(z); sin(z); sin(0.4*t); 1)");
+    IntervalVector x0(4);
+    x0.init(Interval(0,0));
+    Function f_4("x","y","z","x4","(cos(z); sin(z); sin(0.4*x4); 1)");
+    // CAPD integration version
+    TubeVector a_lie_4 = CAPD_integrateODE(domain_4,f_4,x0,timestep_4);
+    cout << "Reference generated " << a_lie_4 << endl;
+    TubeVector reference(domain_4,timestep_4,3); // Generate a 3-dimension reference
+    reference[0] = a_lie_4[0];
+    reference[1] = a_lie_4[1];
+    reference[2] = a_lie_4[2];
+
+    double epsilon = 0.01;
+
+    IntervalVector X0(3);
+    X0[0] = Interval(-0.1,0.1);
+    X0[1] = Interval(-0.1,0.1);
+    X0[2] = Interval(-0.4,0.4);
+
+    lie_group_ex4_separator fullSep(&a_lie_4,&X0);
+
+    IntervalVector x({{-4,4},{-4,4}});
+    IntervalVector proj({{-6,6},{0,15}});
+    SepProj sepProj(fullSep,proj,timestep_4);
+
+    IntervalVector frame({{-4,4},{-4,4}});
+    ipegenerator::Figure fig_4(frame,150,150);
+    fig_4.set_graduation_parameters(-4, 0.5, -4, 0.5);
+    fig_4.set_number_digits_axis_x(1);
+    fig_4.set_number_digits_axis_y(1);
+
+    auto start = chrono::steady_clock::now();
+    sivia_article(x,sepProj,epsilon,fig_4);
+    auto stop = chrono::steady_clock::now();
+    cout << "elapsed time: " << chrono::duration_cast<chrono::milliseconds>(stop - start).count() << " ms" <<endl;
 
 
-    // Integration using Lie Group
-
-    IntervalVector x0_lie_4({{0.0, 0.0},
-                             {0.,  0.},
-                             {0.,  0.},
-                             {0.,  0.}});
-    TubeVector a_lie_4 = CAPD_integrateODE(domain_4, f_4, x0_lie_4, timestep_4);
-    TubeVector x_lie_4(Interval(0,15), timestep_4,3);
-    TFunction tf_4("(t;t; sin(0.4*t))");
-    TubeVector v_lie_4(Interval(0,15),timestep_4, tf_4);
-    v_lie_4[0] = cos(v_lie_4[2]);
-    v_lie_4[1] = sin(v_lie_4[2]);
-
-    IntervalVector X0_simplified_4({{-0.1, 0.1},
-                                    {-0.1, 0.1},
-                                    {-0.4, 0.4}});
 
 
 
-    // Define all necessary contractors
-    ibex::CtcFwdBwd ctc_sub(*new ibex::Function("x", "y", "z", "x-y-z"));
-    ibex::Variable x_v(4, "x_simp"); // x_simp=(x1,x2,x3,x4)
-    ibex::Variable w_v(4, "w"); // a(x4)
-    ibex::Variable beta_v(1, "beta"); // x3-a3(x4)
-    ibex::Function phi_simplified_4(x_v, w_v, beta_v,
-                                    Return(x_v[0] + cos(beta_v[0]) * (-w_v[0]) - sin(beta_v[0]) * (-w_v[1]),
-                                           x_v[1] + sin(beta_v[0]) * (-w_v[0]) + cos(beta_v[0]) * (-w_v[1]),
-                                           beta_v[0]));
-    ibex::CtcFwdBwd ctc_phi_simp_4(phi_simplified_4, X0_simplified_4);
+    ibex::CtcFwdBwd ctc_sub(*new ibex::Function("x[4]", "y[3]", "z", "x[3]-y[2]-z"));
+    ibex::Function phi("x[4]","w[3]","b","( x[1] + cos(b) * (-w[0]) - sin(b) * (-w[1]) - [-0.1,0.1]; x[2] + sin(b) * (-w[0]) + cos(b) * (-w[1]) - [-0.1,0.1]; b - [-0.4,0.4] )");
+    CtcFunction ctc_phi(phi);
     codac::CtcEval ctc_eval;
+    ctc_eval.preserve_slicing(true);
+    ctc_eval.set_fast_mode();
 
 
-    // simplified version
-    ContractorNetwork cn_lie_4;
+
+    ContractorNetwork cn_simplified_out_4;
     IntervalVectorVar box_simp_out_4(4);
-    IntervalVector& w_simp_out_4 = cn_lie_4.create_interm_var(IntervalVector(4));
-    Interval& beta_simp_out_4 = cn_lie_4.create_interm_var( Interval());
-    cn_lie_4.add(ctc_eval, {box_simp_out_4[3], w_simp_out_4, a_lie_4});
-    cn_lie_4.add(ctc_sub, {box_simp_out_4[2], w_simp_out_4[2], beta_simp_out_4});
-    cn_lie_4.add(ctc_phi_simp_4, {box_simp_out_4, w_simp_out_4, beta_simp_out_4});
-    ctc_cn ctc_cn_lie_4(&cn_lie_4, &box_simp_out_4);
+    IntervalVector& w_simp_out_4 = cn_simplified_out_4.create_interm_var(IntervalVector(3));
+    Interval& beta_simp_out_4 = cn_simplified_out_4.create_interm_var( Interval());
+    cn_simplified_out_4.add(ctc_eval, {box_simp_out_4[0], w_simp_out_4, reference});
+    cn_simplified_out_4.add(ctc_sub, {box_simp_out_4, w_simp_out_4, beta_simp_out_4});
+    cn_simplified_out_4.add(ctc_phi, {box_simp_out_4, w_simp_out_4, beta_simp_out_4});
+    ctc_cn ctc_cn_out_4(&cn_simplified_out_4, &box_simp_out_4);
+    CtcStatic ctc_static(ctc_cn_out_4,true);
+
+    // Initialising output tube
+    TubeVector x_total_4(Interval(0,15), timestep_4,3);
+    TubeVector x_unreachable(Interval(0,15), timestep_4,3);
+    TubeVector x_lie_4(Interval(0,15), timestep_4,3);
+
+    // Generating derivative of output tube
+    TFunction tf_4("(t;t; sin(0.4*t))");
+    TubeVector v_lie_4(Interval(0,15),timestep_4, tf_4); // derivative of tune to contract
+    v_lie_4[0] = cos(x_total_4[2]);
+    v_lie_4[1] = sin(x_total_4[2]);
+    TubeVector v_lie_4_unreachable(Interval(0,15),timestep_4, tf_4); // derivative of tune to contract
 
 
-//######################################################################################################################5
 
+
+    Interval time_domain = x_total_4.tdomain();
+    IntervalVector to_reach({Interval::ALL_REALS,Interval::ALL_REALS,Interval::ALL_REALS});
+    Function f_reachable("x[2]","(sqrt((x[0]-0.6)^2+(x[1]-0.45)^2)-0.1)");
+    //Function f_unreachable("x[2]","(sqrt((x[0]-0.6)^2+(x[1]-0.6)^2)-0.1)"); uncomment for an unreachable area
+    CtcFunction ctc_circle(f_reachable);
+
+
+    // Main contractor network reachability
     ContractorNetwork cn_total;
-    cn_total.add(ctc_eval,{x_lie_4.tdomain(),IntervalVector({{1.1,1.3},{1.1,1.3},{NEG_INFINITY,POS_INFINITY}}),x_lie_4,v_lie_4});
-    cn_total.add(ctc_cn_lie_4,{x_lie_4});
+    cn_total.add(ctc_static,{x_total_4}); // lie constraint
+    cn_total.add(ctc_circle,{to_reach[0],to_reach[1]}); // circle constraint
+    cn_total.add(ctc_eval,{time_domain,to_reach,x_total_4,v_lie_4}); // box to reach
 
-//######################################################################################################################
 
 
     start = chrono::steady_clock::now();
-    cn_total.contract();
+    cn_total.contract(true);
     stop = chrono::steady_clock::now();
     cout << "Reachability ex 4 processed in : "
          << chrono::duration_cast<chrono::milliseconds>(stop - start).count() << " ms" << endl;
 
 
+    ctc_static.contract(x_lie_4);
 
 
-    // Graphics
-    IntervalVector frame_4({{-4, 4},
-                            {-4, 4}});
-    ipegenerator::Figure fig_4(frame_4, 150, 150);
-    fig_4.set_graduation_parameters(-4, 0.5, -4, 0.5);
-    fig_4.set_number_digits_axis_x(1);
-    fig_4.set_number_digits_axis_y(1);
+    codac::ColorMap colorMap_lie(codac::InterpolMode::RGB);
+    codac::rgb yellow= codac::make_rgb((float)1.,(float)1.,(float)0.);
+    codac::rgb magenta= codac::make_rgb((float)1.,(float)0.,(float)1.);
+    colorMap_lie.add_color_point(yellow,0);
+    colorMap_lie.add_color_point(magenta,1);
+    fig_4.set_opacity(5);
+    fig_4.draw_tubeVector(&x_lie_4,"x_lie", 0, 1, &colorMap_lie);
 
-    // Visuals initialization
-    if ( lohner_done )
-    {
-        fig_4.set_color_stroke("red");
-        fig_4.set_color_fill("red");
-        fig_4.set_opacity(30);
-        fig_4.set_color_type(ipegenerator::STROKE_AND_FILL);
-        //fig_4.draw_tubeVector(&x_lohner_4, 0, 1);
-        lohner_done = false;
-    }
-    if ( capd_done )
-    {
-        fig_4.set_color_stroke("blue");
-        fig_4.set_color_fill("blue");
-        fig_4.set_opacity(30);
-        fig_4.set_color_type(ipegenerator::STROKE_AND_FILL);
-        //fig_4.draw_tubeVector(&x_capd_4, 0, 1);
-        capd_done = false;
-    }
-    fig_4.set_opacity(30);
-    fig_4.set_color_stroke("yellow");
-    fig_4.set_color_fill("yellow");
-    fig_4.set_opacity(30);
-    fig_4.set_color_type(ipegenerator::STROKE_AND_FILL);
-    fig_4.draw_tubeVector(&x_lie_4, 0, 1);
-    fig_4.set_opacity(30);
-    fig_4.draw_box(x0_4.subvector(0, 1), "green", "green");
+    codac::ColorMap colorMap(codac::InterpolMode::RGB);
+    codac::rgb red= codac::make_rgb((float)1.,(float)0.,(float)0.);
+    codac::rgb green= codac::make_rgb((float)0.,(float)1.,(float)0.);
+    colorMap.add_color_point(red,0);
+    colorMap.add_color_point(green,1);
+    fig_4.set_opacity(5);
+    fig_4.draw_tubeVector(&x_total_4,"x_total", 0, 1, &colorMap);
+
+    fig_4.add_layer("reached");
+    fig_4.set_current_layer("reached");
     fig_4.set_opacity(100);
-    fig_4.draw_circle(1.2,1.2,0.1);
+    fig_4.set_color_stroke("green");
+    fig_4.set_color_fill("green");
+    fig_4.set_color_type(ipegenerator::STROKE_AND_FILL);
+    fig_4.draw_circle(0.6,0.45,0.1);
+
+    fig_4.draw_tubeVector(&a_lie_4,"a_lie",0,1,"black","black",ipegenerator::STROKE_AND_FILL);
+
+    // To check with unreachable area
+    fig_4.add_layer("unreachable");
+    fig_4.set_current_layer("unreachable");
+    fig_4.set_color_stroke("red");
+    fig_4.set_color_fill("red");
+    fig_4.set_color_type(ipegenerator::STROKE_AND_FILL);
+    fig_4.draw_circle(0.6,0.6,0.1);
+
+
+    fig_4.set_current_layer("data");
+    fig_4.set_opacity(30);
+    fig_4.draw_box(X0.subvector(0, 1), "green", "green");
+    fig_4.set_opacity(100);
     fig_4.add_layer("text");
     fig_4.set_current_layer("text");
     fig_4.set_color_stroke("black");
     fig_4.set_color_type(ipegenerator::STROKE_ONLY);
-    //fig_4.draw_text("\\mathbb{X}_0", 0.4, 2.5, true);
+    fig_4.draw_text("{$\\mathbb{X}_0$}",0,-0.07,false);
     fig_4.draw_axis("x1", "x2");
-    fig_4.save_ipe("reachability.ipe");
-    fig_4.save_pdf("reachability.pdf");
+    fig_4.save_ipe("reachability_static.ipe");
+    fig_4.save_pdf("reachability_static.pdf");
+
+
 }
 
 int main(int argc, char* argv[])
 {
     Tube::enable_syntheses();
-    example_4(true, true);
+    reachability();
 }
